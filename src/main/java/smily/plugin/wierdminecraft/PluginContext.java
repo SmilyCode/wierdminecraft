616ec197898219cb8db38912dbabcf093869ad45
smilykuy <smilykuy8@gmail.com>
2021-08-19 18:06:20 +0700

    Added PlayerInteractEvent and material array for diamond armor

--- src/main/java/smily/plugin/wierdminecraft/PluginContext.java
+++ src/main/java/smily/plugin/wierdminecraft/PluginContext.java
@@ -1,8 +1,11 @@
 package smily.plugin.wierdminecraft;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public final class PluginContext {
-    static ApplicationContext context = new AnnotationConfigApplicationContext(PluginAnnotationConfiguration.class);
+    public static ApplicationContext context = new AnnotationConfigApplicationContext(PluginAnnotationConfiguration.class);
+    public static Plugin plugin = Bukkit.getPluginManager().getPlugin("WierdMinecraft");
 }
--- src/main/java/smily/plugin/wierdminecraft/event/DiamondArmorDeathEvent.java
+++ src/main/java/smily/plugin/wierdminecraft/event/DiamondArmorDeathEvent.java
@@ -1,15 +1,30 @@
 package smily.plugin.wierdminecraft.event;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import smily.plugin.wierdminecraft.PluginContext;
+
+import java.util.stream.Stream;
 
 
 public class DiamondArmorDeathEvent implements Listener {
+
+    Material[] diamondArmor =
+            {Material.DIAMOND_BOOTS
+            , Material.DIAMOND_HELMET
+            , Material.DIAMOND_CHESTPLATE
+            , Material.DIAMOND_LEGGINGS};
+
     boolean checkArmor(Player player){
         int armorMeter = 0;
 
@@ -36,17 +51,42 @@ public class DiamondArmorDeathEvent implements Listener {
     }
 
     @EventHandler
-    public void doEvent(InventoryClickEvent event) {
+    public void inventoryClickEvent(InventoryClickEvent event) {
         if (event.getWhoClicked() instanceof Player) {
 
             Player player = (Player) event.getWhoClicked();
 
             if (player.getGameMode().equals(GameMode.SURVIVAL)){
-                if (event.getClickedInventory().getType().equals(player.getInventory().getType())) {
+                Bukkit.getScheduler().scheduleSyncDelayedTask(PluginContext.plugin, () -> {
                     if (checkArmor(player)) {
                         player.setHealth(0d);
+                        player.sendMessage("You Die");
+                    }
+                }, 5);
+
+                if (event.getClick().equals(ClickType.RIGHT)) {
+                    if (player.getOpenInventory().getType().equals(InventoryType.PLAYER)) {
+                        if (checkArmor(player)) {
+                            player.setHealth(0d);
+                            player.sendMessage("You Die");
+                        }
                     }
                 }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteractEvent(PlayerInteractEvent e){
+        Player player = e.getPlayer();
+        if (e.getHand().equals()) {
+            if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+                Bukkit.getScheduler().scheduleSyncDelayedTask(PluginContext.plugin, () -> {
+                    if (checkArmor(player)) {
+                        player.setHealth(0d);
+                        player.sendMessage("You Die");
+                    }
+                }, 5);
             }
         }
     }
